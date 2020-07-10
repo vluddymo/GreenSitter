38c33f26eade919eb705639ffb9396bcbb99b32a
Wladi Moser <wladi.moser@gmail.com>
2020-07-10 10:50:12 +0200

    Add Spring initializr module

---
+++ backend/src/test/java/de/neuefische/greensitter/GreenSitterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.greensitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenSitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
