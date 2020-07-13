b2a00650ade65f48690d9bbcbe35319e0abecfa3
Wladi Moser <wladi.moser@gmail.com>
2020-07-13 15:51:03 +0200

    Add Plant model

---
+++ backend/src/main/java/de/neuefische/greensitter/model/Plant.java
@@ -0,0 +1,18 @@
+package de.neuefische.greensitter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "plants")
+public class Plant {
+    @Id
+    private String name;
+
+
+}
