3cd52bd0ab08574cbf0a0ce6976d8ef68d63aadc
Wladi Moser <wladi.moser@gmail.com>
2020-07-23 20:23:44 +0200

    Fix Api Url

--- backend/src/main/java/de/neuefische/greensitter/api/ApiSearchService.java
+++ backend/src/main/java/de/neuefische/greensitter/api/ApiSearchService.java
@@ -9,23 +9,18 @@ import org.springframework.web.client.RestTemplate;
 public class ApiSearchService {
 
     @Value("${api.auth.token}")
-    String token;
+    private String token;
 
     public PlantData[] getSearchResults(String query){
 
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://trefle.io/api/v1/species/search?token="+token+"&q="+query+"&filter_not[common_name]=null";
+        String url = "https://trefle.io/api/v1/plants/search?token="+token+"&q="+query;
 
         ResponseEntity<TrefleApiQueryResults> responseEntity = restTemplate.getForEntity(url, TrefleApiQueryResults.class);
 
         TrefleApiQueryResults queryData = responseEntity.getBody();
 
-        if (queryData.getData().length >0) {
-            System.out.println(queryData);
-            return queryData.getData();
-
-        }
-        throw new IllegalStateException("No data from Trefle api");
+        return queryData.getData();
     }
 
 }
