ef7d554fd4420021a26e5a8fda6d7c406c6b63f8
Wladi Moser <wladi.moser@gmail.com>
2020-07-23 13:27:04 +0200

    Change Trefle api fetching Url

--- backend/src/main/java/de/neuefische/greensitter/api/ApiSearchService.java
+++ backend/src/main/java/de/neuefische/greensitter/api/ApiSearchService.java
@@ -14,7 +14,7 @@ public class ApiSearchService {
     public PlantData[] getSearchResults(String query){
 
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://trefle.io/api/v1/plants/search?token="+token+"&q="+query+"&filter_not[common_name]=null&filter_not[image_url]=null";
+        String url = "https://trefle.io/api/v1/species/search?token="+token+"&q="+query+"&filter_not[common_name]=null";
 
         ResponseEntity<TrefleApiQueryResults> responseEntity = restTemplate.getForEntity(url, TrefleApiQueryResults.class);
 
