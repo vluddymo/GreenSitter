6d16d9371ccfbf98d759ccf1f931f6b551e443da
Wladi Moser <wladi.moser@gmail.com>
2020-07-23 20:21:54 +0200

    Create ApiSearchContext

---
+++ frontend/src/context/apiSearch/ApiSearchContext.js
@@ -0,0 +1,4 @@
+import {createContext} from 'react'
+
+export const ApiSearchStateContext= createContext({});
+export const ApiSearchDispatchContext= createContext({});
---
+++ frontend/src/context/apiSearch/ApiSearchContextProvider.js
@@ -0,0 +1,23 @@
+import React, {useReducer} from "react";
+import {ApiSearchDispatchContext, ApiSearchStateContext} from "./ApiSearchContext";
+import apiSearchReducer from "./apiSearchReducer";
+
+
+
+export default function ApiSearchContextProvider({children}) {
+
+  const [state, dispatch] = useReducer(apiSearchReducer, {
+    results: [],
+    fetchStatus: undefined
+  });
+
+  return (
+      <ApiSearchStateContext.Provider value={state}>
+        <ApiSearchDispatchContext.Provider value={dispatch}>
+          {children}
+        </ApiSearchDispatchContext.Provider>
+      </ApiSearchStateContext.Provider>
+  )
+
+
+}
