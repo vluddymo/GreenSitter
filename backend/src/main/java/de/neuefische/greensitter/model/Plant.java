bb5861ae6db5331f861361a080fa89e474e8078d
Wladi Moser <wladi.moser@gmail.com>
2020-07-24 11:54:32 +0200

    Change Plant model

--- backend/src/main/java/de/neuefische/greensitter/model/Plant.java
+++ backend/src/main/java/de/neuefische/greensitter/model/Plant.java
@@ -1,5 +1,6 @@
 package de.neuefische.greensitter.model;
 
+import de.neuefische.greensitter.api.dtos.ImagesDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Document(collection = "plants")
 public class Plant {
+
+
     @Id
-    private String name;
+    private String id;
+    private String nickName;
+    private String commonName;
+    private String scientificName;
+    private String genus;
+    private String familyCommonName;
+    private String imageUrl;
+
+
+
 
 
 }
