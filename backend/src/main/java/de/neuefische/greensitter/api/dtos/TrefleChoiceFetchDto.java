22d5f1538cf223d94a76d416dbfa38b1b6f1e3a5
Wladi Moser <wladi.moser@gmail.com>
2020-07-24 11:46:44 +0200

    Create Dtos for Adding fetch

---
+++ backend/src/main/java/de/neuefische/greensitter/api/dtos/TrefleChoiceFetchDto.java
@@ -0,0 +1,14 @@
+package de.neuefische.greensitter.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrefleChoiceFetchDto {
+
+    private ChoiceFetchData data;
+
+}
---
+++ backend/src/main/java/de/neuefische/greensitter/model/dtos/ChosenPlantDto.java
@@ -0,0 +1,16 @@
+package de.neuefische.greensitter.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChosenPlantDto {
+
+    private String nickName;
+    private String choiceId;
+}
