0f475b319a1a2057d1c4177a5a98c9a499810780
Wladi Moser <wladi.moser@gmail.com>
2020-07-29 18:55:36 +0200

    Expand plantActions.js with deleting functionality

--- frontend/src/context/plant/plantActions.js
+++ frontend/src/context/plant/plantActions.js
@@ -1,5 +1,5 @@
 
-import {fetchAllPlants, putAPlant} from "../../utils/plantsUtils";
+import {fetchAllPlants, putAPlant, removeAPlant} from "../../utils/plantsUtils";
 
 export const FETCH_PLANTS = 'FETCH_PLANTS';
 export const FETCH_PLANTS_SUCCESS = 'FETCH_PLANTS_SUCCESS';
@@ -8,6 +8,10 @@ export const FETCH_PLANTS_FAILED = 'FETCH_PLANTS_FAILED';
 export const ADD_PLANT = 'ADD_PLANT';
 export const ADD_PLANT_SUCCESS = 'ADD_PLANT_SUCCESS';
 export const ADD_PLANT_FAILED = 'ADD_PLANT_FAILED';
+
+export const DELETE_PLANT = 'DELETE_PLANT';
+export const DELETE_PLANT_SUCCESS = 'DELETE_PLANT_SUCCESS';
+export const DELETE_PLANT_FAILED = 'DELETE_PLANT_FAILED';
 
 export async function fetchPlants(dispatch) {
   dispatch({ type: FETCH_PLANTS });
@@ -28,3 +32,15 @@ export async function addPlant(dispatch, choiceData) {
     dispatch({ type: ADD_PLANT_FAILED, payload: error });
   }
 }
+
+export async function removePlant(dispatch, nickName) {
+  dispatch({ type: DELETE_PLANT });
+  try {
+    await removeAPlant(nickName);
+    dispatch({ type: DELETE_PLANT_SUCCESS, payload: nickName });
+  } catch (error) {
+    dispatch({ type: DELETE_PLANT_FAILED });
+  }
+}
+
+
