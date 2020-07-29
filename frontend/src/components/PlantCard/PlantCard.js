ae6ab5358ac15c731e18a1f2378edf5fe31a093d
Wladi Moser <wladi.moser@gmail.com>
2020-07-29 18:56:43 +0200

    Implement deleting fucntionality in PlantCard

--- frontend/src/components/PlantCard/PlantCard.js
+++ frontend/src/components/PlantCard/PlantCard.js
@@ -1,7 +1,7 @@
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import React, {useContext} from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardActions from "@material-ui/core/CardActions";
@@ -11,6 +11,9 @@ import Avatar from "@material-ui/core/Avatar";
 import DeleteIcon from '@material-ui/icons/Delete';
 import green from "@material-ui/core/colors/green";
 import EditIcon from '@material-ui/icons/Edit';
+import {PlantDispatchContext} from "../../context/plant/PlantContext";
+import {removePlant} from "../../context/plant/plantActions";
+import {useHistory} from "react-router-dom";
 
 const useStyles = makeStyles((theme) =>({
   root: {
@@ -43,7 +46,15 @@ const useStyles = makeStyles((theme) =>({
 
 export default function PlantCard({plant}) {
 
+  const history = useHistory();
   const classes = useStyles();
+  const dispatch = useContext(PlantDispatchContext);
+
+  function handleDelete(event) {
+    event.stopPropagation();
+    removePlant(dispatch, plant.nickName)
+        .then(() => (history.push("/")));
+  }
 
 
   return (
@@ -76,7 +87,7 @@ export default function PlantCard({plant}) {
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
-          <IconButton aria-label="delete">
+          <IconButton aria-label="delete" onClick={handleDelete}>
             <DeleteIcon color={"error"} fontSize={"large"}/>
           </IconButton>
         </CardActions>
