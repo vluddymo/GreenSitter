06d543b3458b4b94e9d9f6b7a321658528dd6330
Wladi Moser <wladi.moser@gmail.com>
2020-07-29 17:48:35 +0200

    Write fetch for getting a plant by nickName in frontend

--- frontend/src/utils/plantsUtils.js
+++ frontend/src/utils/plantsUtils.js
@@ -1,5 +1,19 @@
 import {getJWTToken} from "./jwt-utils";
 
+export async function fetchPlantByNickName(nickName) {
+  const token = getJWTToken();
+  const response = await fetch("/api/shelve/"+nickName, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (response.status !== 200) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
+}
+
 export async function fetchAllPlants(){
   const token = getJWTToken();
   const response = await fetch("/api/shelve", {
