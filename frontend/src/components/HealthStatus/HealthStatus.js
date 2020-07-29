f410fc1bd61ab21c5f4d1db4a1856ae428b5a2d6
Wladi Moser <wladi.moser@gmail.com>
2020-07-29 17:54:52 +0200

    Do some more styling

--- frontend/src/components/HealthStatus/HealthStatus.js
+++ frontend/src/components/HealthStatus/HealthStatus.js
@@ -4,11 +4,9 @@ export default function HealthStatus({percentage}) {
 
   return (
 
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
-        <g>
-          <rect width="100" height="100" x={"20"} y={"40"} fill={"black"} />
-          <rect width="100" height={percentage} x={"20"} y={"40"} fill={"blue"} />
-        </g>
+      <svg width={"100%"} height={"100%"}>
+          <rect width={"100%"} height={"100%"} fill={"blue"} />
+          <rect width={"100%"} height={100-percentage+"%"} fill={"lightblue"} />
       </svg>
 
   )
