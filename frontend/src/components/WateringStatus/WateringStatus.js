094f5ac2e3056c68e48c993e028e0684f88082a8
Wladi Moser <wladi.moser@gmail.com>
2020-08-03 13:21:24 +0200

    Delete unused function to fix CD

--- frontend/src/components/WateringStatus/WateringStatus.js
+++ frontend/src/components/WateringStatus/WateringStatus.js
@@ -10,21 +10,6 @@ const useStyles = makeStyles({
   }
 })
 
-function statusColorDefinition(status) {
-
-  switch (status) {
-    case (status >= 80):
-      return "#025ef8";
-    case (status >= 50):
-      return "#2474fa";
-    case (status >= 35):
-      return "#6ea0f5";
-    default:
-      console.log(status)
-      return "#f8ca57";
-  }
-
-}
 
 export default function WateringStatus({wateringStatus}) {
 
