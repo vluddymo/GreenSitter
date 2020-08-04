bc73cea82d76bf48591e549f3c18193c5130c4e3
Wladi Moser <wladi.moser@gmail.com>
2020-08-04 15:49:05 +0200

    Try to fix pushing

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -40,7 +40,6 @@ const useStyles = makeStyles((theme) => ({
     alignSelf: "center",
     justifyContent: "center",
     boxShadow: '-5px -5px 5px rgba(255,255,255,0.7) inset, 5px 5px 5px rgba(174,170,192,0.2) inset',
-    border: "none"
 
   },
   loginButton: {
@@ -51,7 +50,6 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 10,
     fontWeight: "normal",
     alignSelf: "center",
-    border: "none"
   },
   textField: {
     alignSelf: "center",
