96b301fcc99ec3ea6ef93819fc69f50cd6ee5997
Wladi Moser <wladi.moser@gmail.com>
2020-08-06 23:12:22 +0200

    Refactor code into components

--- frontend/src/pages/PlantsDashboard.js
+++ frontend/src/pages/PlantsDashboard.js
@@ -4,36 +4,16 @@ import {fetchPlants} from "../context/plant/plantActions";
 import {PlantDispatchContext, PlantStateContext} from "../context/plant/PlantContext";
 import Typography from "@material-ui/core/Typography";
 import LoadingSpinner from "../components/Spinner/LoadingSpinner";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import Box from "@material-ui/core/Box";
-import PageTitle from "../components/Containers/PageComponents/PageTitle/PageTitle";
-import PageContent from "../components/Containers/PageComponents/PageContent/PageContent";
+import PageTitle from "../components/PageComponents/PageTitle/PageTitle";
+import PageContent from "../components/PageComponents/PageContent/PageContent";
 import WateringButton from "../components/Buttons/FabButtons/WateringButton";
 import AddingButton from "../components/Buttons/FabButtons/AddingButton";
+import ButtonBox from "../components/Buttons/FabButtons/ButtonBox/ButtonBox";
 
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    justifyContent: "flex-start",
-    height: "min-content",
-  },
-  buttonBox: {
-    position: 'absolute',
-    zIndex: 1,
-    bottom: 20,
-    left: 0,
-    right: 0,
-    margin: 'auto',
-    display: "flex",
-    justifyContent: "space-around",
-    width: "50%",
-  },
-});
 
 export default function PlantsDashboard() {
 
-  const classes = useStyles();
   const {plants, fetchStatus} = useContext(PlantStateContext);
   const dispatch = useContext(PlantDispatchContext);
 
@@ -59,10 +39,10 @@ export default function PlantsDashboard() {
                 Fetch Plants failed
               </Typography>
           )}
-          <Box className={classes.buttonBox}>
+          <ButtonBox>
             <AddingButton/>
             <WateringButton/>
-          </Box>
+          </ButtonBox>
       </PageContent>
   )
 }
